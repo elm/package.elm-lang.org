c21b286aedd0804dfd5b1d68e061072c0500591f
Evan Czaplicki <info@elm-lang.org>
2014-11-06 03:33:43 -0800

    Begin simple version of the package listing

--- frontend/Catalog.elm
+++ frontend/Catalog.elm
@@ -1,53 +1,33 @@
 module Catalog where
 
-import String
-import Http
-import Json
-import JavaScript.Experimental as JSE
-import Text
+import Basics (..)
+import Color
 import ColorScheme as C
-import Skeleton (skeleton)
+import Json
+import Graphics.Element (..)
+import List
+import Signal
+import String
+import Text
 import Window
 
-jsonResponse = Http.sendGet (constant "/libraries.json")
+import TopBar
 
-addSearchText library =
-    { library | searchText = String.toLower <| library.name ++ " " ++ library.summary }
 
-format : Http.Response String -> [Library]
-format response =
-    case response of
-      Http.Success str ->
-          case Json.fromString str of
-            Just (Json.Array xs) ->
-                map (addSearchText << JSE.toRecord << JSE.fromJson) xs
-            _ -> []
-      _ -> []
+main : Signal.Signal Element
+main =
+    Signal.map scene Window.dimensions
 
-type Library = { name:String, summary:String, searchText:String, versions:[String] }
 
-search : String -> [Library] -> [Library]
-search term libraries =
-    let lowTerm = String.toLower term in
-    if String.length term < 2 then libraries else
-        filter (String.contains lowTerm << .searchText) libraries
+search : Signal.Channel TopBar.Update
+search =
+    Signal.channel TopBar.NoOp
 
-libraries : Signal [Library]
-libraries = format <~ jsonResponse
 
-deslash = String.map (\c -> if c == '/' then '-' else c)
-
-row : Int -> Library -> Element
-row w library =
-    let url = "/catalog/" ++ deslash library.name ++ "/" ++ head library.versions in
-    flow down
-    [ color C.mediumGrey <| spacer w 1
-    , flow right [ container 300 36 midLeft (leftAligned << Text.link url <| toText library.name)
-                 , container (w-300) 36 midLeft (plainText library.summary)
-                 ]
-    ]
-
-scene term libraries w =
-    flow down <| map (row w) (search term libraries)
-
-main = skeleton "filter" [] scene libraries
+scene : (Int,Int) -> Element
+scene (windowWidth, windowHeight) =
+  color C.background <|
+  flow down
+  [ TopBar.view windowWidth search (TopBar.Model TopBar.Global "map" TopBar.Hover)
+  , spacer windowWidth windowHeight
+  ]
