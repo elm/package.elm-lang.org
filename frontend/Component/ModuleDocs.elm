ba09c5c9e1e91a97acdedff18944c11dbf1e2679
Evan Czaplicki <info@elm-lang.org>
2014-11-06 16:44:20 -0800

    Add rudimentary parser of module documentation

--- frontend/Component/ModuleDocs.elm
+++ frontend/Component/ModuleDocs.elm
@@ -1,11 +1,17 @@
 module Component.ModuleDocs where
 
 import Basics (..)
+import Char
 import ColorScheme as C
+import Dict
+import Debug
 import Graphics.Element (..)
+import List
+import List ((::), (++))
 import Markdown
 import Maybe
-import List ((++))
+import Regex
+import String
 import Text
 
 
@@ -23,7 +29,7 @@ view innerWidth user package docs =
     [ header
     , color C.lightGrey (spacer innerWidth 1)
     , spacer innerWidth 12
-    , width innerWidth (Markdown.toElement docs.comment)
+    , viewDocs innerWidth documentation docs.comment
     ]
 
 
@@ -39,14 +45,113 @@ dummyDocs =
 comment = """
 basic functions for stuff
 
-@docs identity
+@docs identity, always
+
+basic functions for stuff
+
+@docs identity, always
 """
+
+documentation : Dict.Dict String String
+documentation =
+    Dict.fromList [("identity", identityComment), ("always", alwaysComment)]
 
 identityComment = """
 Function that returns the argument unaltered
 
     identity 42 == 42
 """
+
+alwaysComment = """
+Function that always returns the same value
+
+    always 42 "hats" == 42
+    always 42 123456 == 42
+    always 42 [3..8] == 42
+"""
+
+
+viewDocs : Int -> Dict.Dict String String -> String  -> Element
+viewDocs innerWidth documentation comment =
+  let (prose :: chunks) =
+        String.split "\n@docs " comment
+
+      varProsePairs : [([String], String)]
+      varProsePairs =
+        List.map (extractVars [] Space) chunks
+  in
+      flow down <|
+        viewProse innerWidth prose
+        :: List.concatMap (viewPair innerWidth documentation) varProsePairs
+
+
+type ExtractState = Variable String | Comma | Space
+
+extractVars : [String] -> ExtractState -> String -> ([String], String)
+extractVars vars state chunk =
+  case String.uncons chunk of
+    Maybe.Nothing ->
+      case state of
+        Variable x ->
+          (List.reverse (String.reverse x :: vars), "")
+        _ ->
+          (List.reverse vars, "")
+
+    Maybe.Just (c, rest) ->
+      case state of
+        Variable x ->
+          if  | isVarChar c ->
+                  extractVars vars (Variable (String.cons c x)) rest
+              | String.any ((==) c) " \n\t\r" ->
+                  extractVars (String.reverse x :: vars) Comma rest
+              | c == ',' ->
+                  extractVars (String.reverse x :: vars) Space rest
+              | otherwise ->
+                  (List.reverse (String.reverse x :: vars), chunk)
+
+        Comma ->
+          if  | String.any ((==) c) " \n\t\r" ->
+                  extractVars vars Comma rest
+              | c == ',' ->
+                  extractVars vars Space rest
+              | otherwise ->
+                  (List.reverse vars, chunk)
+
+        Space ->
+          if  | isVarChar c ->
+                  extractVars vars (Variable (String.cons c "")) rest
+              | String.any ((==) c) " \n\t\r" ->
+                  extractVars vars Space rest
+              | otherwise ->
+                  (List.reverse vars, chunk)
+
+
+isVarChar : Char -> Bool
+isVarChar c =
+    Char.isLower c || Char.isUpper c || Char.isDigit c || c == '_' || c == '\''
+
+
+docsPattern : Regex.Regex
+docsPattern =
+    Regex.regex "(.*)\n@docs\\s+([a-zA-Z0-9_']+(?:,\\s*[a-zA-Z0-9_']+)*)"
+
+
+viewPair : Int -> Dict.Dict String String -> ([String], String) -> [Element]
+viewPair innerWidth documentation (vars, prose) =
+    List.map (viewVar innerWidth documentation) vars
+    ++ [viewProse innerWidth prose]
+
+
+viewProse : Int -> String -> Element
+viewProse innerWidth prose =
+    width innerWidth (Markdown.toElement prose)
+
+
+viewVar : Int -> Dict.Dict String String -> String -> Element
+viewVar innerWidth documentation var =
+    case Dict.get var documentation of
+      Maybe.Just str -> viewProse innerWidth str
+      Maybe.Nothing -> Debug.crash "everything should be in the dictionary"
 
 
 -- MODEL OF DOCUMENTATION
