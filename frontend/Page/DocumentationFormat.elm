607a6ba99398272f0c8e1d4afa7a90e4406708b5
Joshua Luke <joshuadluke@gmail.com>
2015-01-27 10:32:19 -0500

    Fix links to String and Maybe docs

--- frontend/Page/DocumentationFormat.elm
+++ frontend/Page/DocumentationFormat.elm
@@ -46,7 +46,7 @@ documentation for complete examples.
 
 ## Documenting a value
 
-Here is an example from [the `String` library](/catalog/elm-lang-Elm/latest/String):
+Here is an example from [the `String` library](/packages/elm-lang/core/latest/String):
 
 ```haskell
 {-| Convert a list of characters into a String. Can be useful if you
@@ -95,7 +95,7 @@ module Maybe (Maybe(..), maybe, isJust, isNothing, map) where
 ```
 
 This represents the text that actually gets displayed as [the
-documentation](/catalog/elm-lang-Elm/latest/Maybe) for a module. Notice that:
+documentation](/packages/elm-lang/core/latest/Maybe) for a module. Notice that:
 
   * The module documentation comes after the module declaration, but
     before the imports. This is so the first thing in the file is the
@@ -103,7 +103,7 @@ documentation](/catalog/elm-lang-Elm/latest/Maybe) for a module. Notice that:
   * The first line starts after a single space, and all subsequent lines
     start in the zeroth column.
   * The `@docs` keyword starts a list of values that are inlined in [the
-    resulting documentation](/catalog/elm-lang-Elm/latest/Maybe).
+    resulting documentation](/packages/elm-lang/core/latest/Maybe).
   * Functions are grouped into related units with titles
   * Although documentation for each function should be self-contained,
     things are ordered intelligently. Assume people will read through
