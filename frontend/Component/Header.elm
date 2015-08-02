b5af172af682d0feb43c9aad59db4c373e80c6b0
Evan Czaplicki <info@elm-lang.org>
2015-08-01 21:22:21 -0700

    Improve code style and visual style

--- frontend/Component/Header.elm
+++ frontend/Component/Header.elm
@@ -5,6 +5,8 @@ import Graphics.Element exposing (..)
 import Html
 import Text
 
+import ColorScheme as C
+
 
 view : Signal.Address String -> Int -> String -> String -> String -> List String -> Maybe String -> Element
 view versionChan innerWidth user package version versions maybeModule =
@@ -41,25 +43,26 @@ view versionChan innerWidth user package version versions maybeModule =
         |> Text.link githubLink
         |> Text.height 12
         |> centered
+
+    toWarning latestVersion =
+      if version == latestVersion then
+        []
+      else
+        [ color C.lightGrey (spacer innerWidth 1)
+        , container innerWidth 40 middle <| centered <|
+            Text.fromString "Warning! The latest version of this package is "
+            ++ Text.link ("/packages/" ++ user ++ "/" ++ package ++ "/" ++ latestVersion) (Text.fromString latestVersion)
+        ]
   in
     flow down <|
-    [ flow right
-      [ container (innerWidth - 100) 100 midLeft bigWords
-      , container 100 100 middle <|
-          flow down
-            [ Html.toElement 100 30 (dropdown versionChan version versions)
-            , width 100 viewSource
-            ]
+      [ flow right
+        [ container (innerWidth - 100) 100 midLeft bigWords
+        , container 100 100 middle <|
+            flow down
+              [ Html.toElement 100 30 (dropdown versionChan version versions)
+              , width 100 viewSource
+              ]
+        ]
       ]
-    ]
-    ++
-    case List.head versions of
-      Nothing -> []
-      Just latestVersion ->
-        if version == latestVersion
-        then []
-        else [ container innerWidth 25 topLeft
-               (leftAligned (Text.fromString "The latest version of this package is: "
-                             ++ Text.link ("/packages/" ++ user ++ "/" ++ package ++ "/" ++ latestVersion)
-                                          (Text.fromString latestVersion))) ]
+      ++ Maybe.withDefault [] (Maybe.map toWarning (List.head versions))
 
