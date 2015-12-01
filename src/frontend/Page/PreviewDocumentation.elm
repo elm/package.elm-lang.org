a373e8d65d2e2e34fad53f32e04dc84b8421cbf9
Evan Czaplicki <info@elm-lang.org>
2015-12-01 14:10:14 -0800

    Split preview out into component, clean things up a bit

--- src/frontend/Page/PreviewDocumentation.elm
+++ src/frontend/Page/PreviewDocumentation.elm
@@ -1,20 +1,16 @@
 module Page.PreviewDocumentation where
 
-import Dict
-import Effects as Fx
+import Effects as Fx exposing (Effects)
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
-import Json.Decode as Json exposing ((:=))
+import Json.Decode as Json
 import StartApp
 import Task
 
-import Route
 import Component.Header as Header
-import Component.PackageDocs as PDocs
+import Component.PackagePreview as Preview
 import Docs.Package as Docs
-import Utils.Markdown as Markdown
-import Utils.Path as Path
+import Route
 
 
 
@@ -47,30 +43,44 @@ docsUploads =
   let
     toDocs body =
       case Json.decodeString Docs.decodePackage body of
-        Err msg ->
-          Fail (Just msg)
+        Err _ ->
+          Preview.Fail (Just "Could not parse file contents as Elm docs.")
 
         Ok dict ->
-          LoadDocs dict
+          Preview.LoadDocs dict
   in
-    Signal.map toDocs uploads
+    Signal.map (UpdatePreview << toDocs) uploads
 
 
 
 -- MODEL
 
 
-type Model
-    = AwaitingFile
-    | BadFile (Maybe String)
-    | GoodFile (Dict.Dict String Docs.Module) PDocs.Model
+type alias Model =
+    { header : Header.Model
+    , preview : Preview.Model
+    }
 
 
-init : (Model, Fx.Effects Action)
+
+-- INIT
+
+
+init : (Model, Effects Action)
 init =
-  ( AwaitingFile
-  , Fx.none
-  )
+  let
+    (header, headerFx) =
+      Header.init Route.Help
+
+    (preview, previewFx) =
+      Preview.init
+  in
+    ( Model header preview
+    , Fx.batch
+        [ headerFx
+        , Fx.map UpdatePreview previewFx
+        ]
+    )
 
 
 
@@ -78,164 +88,28 @@ init =
 
 
 type Action
-    = NoOp
-    | Fail (Maybe String)
-    | LoadDocs (Dict.Dict String Docs.Module)
-    | SwitchTo String
+    = UpdatePreview Preview.Action
 
 
-update : Action -> Model -> ( Model, Fx.Effects Action )
+update : Action -> Model -> (Model, Effects Action)
 update action model =
-  flip (,) Fx.none <|
   case action of
-    NoOp ->
-      model
-
-    Fail maybeMsg ->
-      BadFile maybeMsg
-
-    LoadDocs docs ->
-      case List.head (Dict.keys docs) of
-        Nothing ->
-          BadFile (Just "The JSON you uploaded does not have any modules in it!")
-
-        Just moduleName ->
-          GoodFile docs (docsForModule moduleName docs)
-
-    SwitchTo moduleName ->
-      case model of
-        GoodFile docs _ ->
-          GoodFile docs (docsForModule moduleName docs)
-
-        _ ->
-          model
+    UpdatePreview act ->
+        let
+          (newPreview, fx) =
+            Preview.update act model.preview
+        in
+          ( { model | preview = newPreview }
+          , Fx.map UpdatePreview fx
+          )
 
 
 
 -- VIEW
 
 
-(=>) = (,)
-
-
 view : Signal.Address Action -> Model -> Html
-view address model =
-  div [] <|
-    case model of
-      AwaitingFile ->
-        [ instructions long
-        ]
+view addr model =
+  Header.view addr model.header
+    (Preview.view (Signal.forwardTo addr UpdatePreview) model.preview)
 
-      BadFile maybeMsg ->
-        let
-          errorMsg =
-            case maybeMsg of
-              Just msg ->
-                "Problem uploading that file: " ++ msg
-
-              Nothing ->
-                "Problem uploading that file, try it a different way."
-        in
-          [ instructions long
-          , p [ style [ "color" => "red" ] ] [ text errorMsg ]
-          ]
-
-      GoodFile docs info ->
-        [ instructions short
-        , div
-            [ style
-                [ "border-top" => "1px solid #eeeeee"
-                , "margin-top" => "1em"
-                ]
-            ]
-            [ PDocs.view (Signal.forwardTo address (\_ -> Debug.crash "TODO")) info
-            , viewSidebar address (Dict.keys docs)
-            ]
-        ]
-
-
-viewSidebar : Signal.Address Action -> List String -> Html
-viewSidebar address modulesNames =
-  div [ class "pkg-nav" ]
-    [ ul
-      [ class "pkg-nav-value" ]
-      (moduleLinks address modulesNames)
-    ]
-
-
-moduleLinks : Signal.Address Action -> List String -> List Html
-moduleLinks address modulesNames =
-  let
-    moduleItem moduleName =
-      li [] [ moduleLink address moduleName ]
-  in
-    List.map moduleItem modulesNames
-
-
-moduleLink : Signal.Address Action -> String -> Html
-moduleLink address moduleName =
-  a
-    [ onClick address (SwitchTo moduleName)
-    , class "pkg-nav-module"
-    , href ("#" ++ Path.hyphenate moduleName)
-    ]
-    [ text moduleName ]
-
-
-
--- DOCS FUNCTIONS
-
-
-docsForModule : String -> Dict.Dict String Docs.Module -> PDocs.Model
-docsForModule moduleName docs =
-  case Dict.get moduleName docs of
-    Just moduleDocs ->
-      let
-        chunks =
-          PDocs.toChunks moduleDocs
-            |> List.map (PDocs.chunkMap PDocs.stringToType)
-      in
-        PDocs.ParsedDocs (PDocs.Info moduleName (PDocs.toNameDict docs) chunks)
-
-    Nothing ->
-      PDocs.Loading
-
-
-
--- VIEW INSTRUCTIONS
-
-
-instructions : String -> Html
-instructions md =
-  div
-    [ style [ "width" => "600px" ]
-    ]
-    [ Markdown.block md
-    , input [ type' "file", id "fileLoader" ] []
-    ]
-
-
-long : String
-long = """
-
-# Documentation Preview
-
-To preview your docs, run this command in the root of your package:
-
-```bash
-elm make --docs documentation.json
-```
-
-That will create a file called `documentation.json`. Give me that file.
-
-"""
-
-
-short : String
-short = """
-
-# Documentation Preview
-
-Run `elm make --docs documentation.json` and upload new versions:
-
-"""
