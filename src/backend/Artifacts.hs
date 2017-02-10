1c95c51b9e8ae027b3d4a3b2c9120efcae666acd
Evan Czaplicki <evancz@users.noreply.github.com>
2017-02-10 14:48:12 -0800

    Clean up Artifacts and add `compile` logic
    This moves some code from Main over to Artifacts. This way all of that
    stuff is in one place. It also adds the `url` function so the slashes
    are sure to be right even if someone is running this on windows, which
    would be really weird.

--- src/backend/Artifacts.hs
+++ src/backend/Artifacts.hs
@@ -1,15 +1,76 @@
-module Path where
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
+module Artifacts
+  ( directory
+  , path
+  , url
+  , compile
+  )
+  where
 
+import Control.Monad.Except (ExceptT, runExceptT)
+import qualified Data.Text as Text
+import System.Directory (createDirectoryIfMissing)
+import System.Exit (exitFailure)
 import System.FilePath ((</>), (<.>))
+import System.IO (hPutStrLn, stderr)
 
-import Elm.Compiler.Module as Module
-
-
-artifactDirectory :: FilePath
-artifactDirectory =
-    "artifacts"
+import qualified Elm.Compiler.Module as Module
+import qualified Elm.Utils as Utils
 
 
-artifact :: Module.Raw -> FilePath
-artifact name =
-    artifactDirectory </> Module.hyphenate name <.> "js"
+
+-- PATHS
+
+
+directory :: FilePath
+directory =
+  "artifacts"
+
+
+path :: Module.Raw -> FilePath
+path name =
+  directory </> Text.unpack (Module.hyphenate name) <.> "js"
+
+
+url :: Module.Raw -> FilePath
+url name =
+  directory ++ "/" ++ Text.unpack (Module.hyphenate name) ++ ".js"
+
+
+
+-- COMPILE
+
+
+compile :: IO ()
+compile =
+  do  createDirectoryIfMissing True "artifacts"
+      result <- runExceptT (mapM runElmMake publicModules)
+      case result of
+        Right _ ->
+          return ()
+
+        Left msg ->
+          do  hPutStrLn stderr msg
+              exitFailure
+
+
+runElmMake :: Module.Raw -> ExceptT String IO String
+runElmMake name =
+  Utils.run "elm-make"
+    [ "src" </> "frontend" </> Module.nameToPath name <.> "elm"
+    , "--yes"
+    , "--output=" ++ path name
+    ]
+
+
+publicModules :: [Module.Raw]
+publicModules =
+  [ "Page.Catalog"
+  , "Page.DesignGuidelines"
+  , "Page.DocumentationFormat"
+  , "Page.PreviewDocumentation"
+  , "Page.NotFound"
+  , "Page.Package"
+  , "Page.PackageOverview"
+  ]
