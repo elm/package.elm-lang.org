1005618bb7a4e6b05a1c22c115ccbc424d62b3e6
Evan Czaplicki <evancz@users.noreply.github.com>
2017-06-21 12:18:08 -0700

    Make the compiler happy with package registration code

--- src/backend/Package/Register.hs
+++ src/backend/Package/Register.hs
@@ -1,13 +1,30 @@
+{-# OPTIONS_GHC -Wall #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Package.Register (register) where
 
+import Control.Monad (when)
+import Control.Monad.Trans (liftIO)
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.Time.Clock.POSIX as Time
+import qualified Data.Map as Map
+import qualified Data.Maybe as Maybe
 import Data.Text (Text)
 import qualified Data.Text as Text
 import qualified Data.Text.Encoding as Text
-import Snap.Util.FileUploads
+import qualified Snap.Core as Snap
+import qualified Snap.Util.FileUploads as Snap
+import qualified System.Directory as Dir
+import System.FilePath ((</>))
+import qualified System.IO as IO
+import qualified System.IO.Streams as Stream
 
+import qualified Elm.Package as Pkg
 import qualified GitHub
+import qualified Json.Decode as Decode
+import qualified Memory
 import qualified Package.Path as Path
 import qualified Package.Releases as Releases
+import qualified Server.Error as Error
 
 
 {- Be sure to check:
@@ -20,11 +37,13 @@ import qualified Package.Releases as Releases
 
 -}
 
-register :: Memory -> Snap ()
-register memory =
+register :: GitHub.Token -> Memory.Memory -> Snap.Snap ()
+register token memory =
   do  sha <- getQueryParam "sha"
       name <- verifyName =<< getQueryParam "name"
-      (version, time) <- verifyVersion name sha memory =<< getQueryParam "version"
+      version <- verifyVersion token memory name sha =<< getQueryParam "version"
+
+      time <- liftIO Time.getPOSIXTime
 
       uploadFiles name version time
 
@@ -34,17 +53,17 @@ register memory =
       return ()
 
 
-getQueryParam :: BS.ByteString -> Snap Text
+getQueryParam :: BS.ByteString -> Snap.Snap Text
 getQueryParam param =
-  do  maybeValue <- getParam param
+  do  maybeValue <- Snap.getParam param
       case maybeValue of
         Nothing ->
-          httpError 400 $ "I need a `" ++ BS.unpack param ++ "` query parameter."
+          Error.string 400 $ "I need a `" ++ BS.unpack param ++ "` query parameter."
 
         Just bits ->
           case Text.decodeUtf8' bits of
             Left _ ->
-              httpError 400 $ "The value of query parameter `" ++ BS.unpack param ++ "` is not valid UTF-8."
+              Error.string 400 $ "The value of query parameter `" ++ BS.unpack param ++ "` is not valid UTF-8."
 
             Right value ->
               return value
@@ -54,14 +73,14 @@ getQueryParam param =
 -- VERIFY NAME
 
 
-verifyName :: Text -> Snap Pkg.Name
+verifyName :: Text -> Snap.Snap Pkg.Name
 verifyName rawName =
   case Pkg.fromText rawName of
     Right name ->
       return name
 
     Left problem ->
-       httpStringError 400 (badNameMessage rawName problem)
+       Error.string 400 (badNameMessage rawName problem)
 
 
 badNameMessage :: Text -> String -> String
@@ -81,20 +100,20 @@ badNameMessage name problem =
 -- VERIFY VERSION
 
 
-verifyVersion :: Pkg.Name -> Text -> Text -> Memory -> Snap (Pkg.Version, Time.NominalDiffTime)
-verifyVersion name sha rawVersion memory =
+verifyVersion :: GitHub.Token -> Memory.Memory -> Pkg.Name -> Text -> Text -> Snap.Snap Pkg.Version
+verifyVersion token memory name sha rawVersion =
   case Pkg.versionFromText rawVersion of
     Left problem ->
-      httpStringError 400 $
+      Error.string 400 $
         "I was given an invalid version: " ++ Text.unpack rawVersion ++ "\n\n" ++ problem
 
     Right version ->
       do  verifyIsNew memory name version
-          time <- verifySha version sha
-          return (version, time)
+          verifySha token name version sha
+          return version
 
 
-verifyIsNew :: Memory -> Pkg.Name -> Pkg.Version -> Snap ()
+verifyIsNew :: Memory.Memory -> Pkg.Name -> Pkg.Version -> Snap.Snap ()
 verifyIsNew memory name vsn =
   do  pkgs <- Memory.getPackages memory
       case Map.lookup name pkgs of
@@ -102,15 +121,38 @@ verifyIsNew memory name vsn =
           return ()
 
         Just versions ->
-          when (elem vsn versions) $ httpStringError 400 $
+          when (elem vsn versions) $ Error.string 400 $
             "Version " ++ Pkg.versionToString vsn ++ " has already been published."
 
 
-verifySha :: Token -> Pkg.Name -> Pkg.Version -> Text -> Snap ()
+verifySha :: GitHub.Token -> Pkg.Name -> Pkg.Version -> Text -> Snap.Snap ()
 verifySha token name version sha =
-  do  githubSha <- GitHub.getSha token name version
-      when (sha /= githubSha) $ httpStringError 400 $
+  do  githubSha <- getSha token name version
+      when (sha /= githubSha) $ Error.string 400 $
         "The commit tagged on github as " ++ Pkg.versionToString version ++ " is not the one I was expecting."
+
+
+getSha :: GitHub.Token -> Pkg.Name -> Pkg.Version -> Snap.Snap Text
+getSha token name version =
+  do  response <- liftIO $ GitHub.fetch token $
+        "/repos/" ++ Pkg.toFilePath name ++ "/git/refs/tags/" ++ Pkg.versionToString version
+
+      case response of
+        Left _ ->
+          Error.bytestring 500 "Request to GitHub API failed."
+
+        Right body ->
+          case Decode.parse tagDecoder body of
+            Right sha ->
+              return sha
+
+            Left _ ->
+              Error.bytestring 500 "Bad JSON from GitHub"
+
+
+tagDecoder :: Decode.Decoder Text
+tagDecoder =
+  Decode.at ["object","sha"] Decode.text
 
 
 
@@ -127,54 +169,54 @@ verifySha token name version sha =
         time
 
 -}
-uploadFiles :: Pkg.Name -> Pkg.Version -> Time.NominalDiffTime -> Snap ()
+uploadFiles :: Pkg.Name -> Pkg.Version -> Time.POSIXTime -> Snap.Snap ()
 uploadFiles name version time =
-  do  let dir = Path.dir name version
-      liftIO (createDirectoryIfMissing True dir)
-      results <- handleMultipart defaultUploadPolicy (handlePart dir)
+  do  let dir = Path.directory name version
+      liftIO (Dir.createDirectoryIfMissing True dir)
+      results <- Snap.handleMultipart Snap.defaultUploadPolicy (handlePart dir)
       case Maybe.catMaybes results of
         [] ->
-          liftIO $ writeFile (dir </> "time") (show (floor time))
+          liftIO $ writeFile (dir </> "time") (show (floor time :: Integer))
 
         problems ->
-          do  liftIO (removeDirectoryRecursive dir)
-              httpStringError 404 $
+          do  liftIO (Dir.removeDirectoryRecursive dir)
+              Error.string 404 $
                 "Failure uploading your package:\n" ++ concatMap ("\n  - " ++) problems
 
 
-handlePart :: FilePath -> PartInfo -> InputStream ByteString -> IO (Maybe String)
+handlePart :: FilePath -> Snap.PartInfo -> Stream.InputStream BS.ByteString -> IO (Maybe String)
 handlePart dir info stream =
-  case partFieldName info of
-    "commit-hash" | partDisposition info == DispositionFormData ->
+  case Snap.partFieldName info of
+    "commit-hash" | Snap.partDisposition info == Snap.DispositionFormData ->
       boundedWrite dir "commit-hash" stream
 
-    "elm.json" | partDisposition info == DispositionFile ->
+    "elm.json" | Snap.partDisposition info == Snap.DispositionFile ->
       boundedWrite dir "elm.json" stream
 
-    "README.md" | partDisposition info == DispositionFile ->
+    "README.md" | Snap.partDisposition info == Snap.DispositionFile ->
       boundedWrite dir "README.md" stream
 
-    "documentation.json" | partDisposition info == DispositionFile ->
+    "documentation.json" | Snap.partDisposition info == Snap.DispositionFile ->
       boundedWrite dir "documentation.json" stream
 
     name ->
       return $ Just $ "Did not recognize " ++ show name ++ " part in form-data"
 
 
-boundedWrite :: FilePath -> FilePath -> InputStream ByteString -> IO (Maybe String)
+boundedWrite :: FilePath -> FilePath -> Stream.InputStream BS.ByteString -> IO (Maybe String)
 boundedWrite dir path stream =
   IO.withBinaryFile (dir </> path) IO.WriteMode $ \handle ->
     boundedWriteHelp path handle 0 stream
 
 
-boundedWriteHelp :: FilePath -> Handle -> Int -> InputStream ByteString -> IO (Maybe String)
-boundedWriteHelp path handle bits stream =
-  if bits > 2^17 then
+boundedWriteHelp :: FilePath -> IO.Handle -> Int -> Stream.InputStream BS.ByteString -> IO (Maybe String)
+boundedWriteHelp path handle size stream =
+  if maxSize < size then
     return $ Just $
       "Your " ++ path ++ " is too big. Must be less than 128kb. Let us know if this limit is too low!"
 
   else
-    do  maybeChunk <- Stream.read
+    do  maybeChunk <- Stream.read stream
         case maybeChunk of
           Nothing ->
             return Nothing
@@ -183,3 +225,7 @@ boundedWriteHelp path handle bits stream =
             do  BS.hPut handle chunk
                 boundedWriteHelp path handle (BS.length chunk + size) stream
 
+
+maxSize :: Int
+maxSize =
+  2 ^ (17 :: Int)
