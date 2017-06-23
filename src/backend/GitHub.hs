055ffe279165da0fb64632a35a1bf5dc8f887068
Evan Czaplicki <evancz@users.noreply.github.com>
2017-06-23 10:09:09 -0700

    Add necessary headers for GitHub API

--- src/backend/GitHub.hs
+++ src/backend/GitHub.hs
@@ -5,9 +5,11 @@ module GitHub (Token, init, fetch) where
 
 import Prelude hiding (init)
 import Control.Exception (SomeException, catch)
-import qualified Data.ByteString.Lazy.Char8 as BS
+import qualified Data.ByteString.Char8 as BS
+import qualified Data.ByteString.Lazy as LBS
 import qualified Network.HTTP.Client as Http
 import qualified Network.HTTP.Client.TLS as Http
+import Network.HTTP.Types.Header (hAuthorization, hUserAgent)
 
 
 
@@ -31,8 +33,8 @@ init githubToken =
       let token = Token manager githubToken
       response <- fetch token "/"
       case response of
-        Left _ ->
-          error "Bad OAuth access token for GitHub"
+        Left err ->
+          error $ "Bad OAuth access token for GitHub.\n" ++ err
 
         Right _ ->
           return token
@@ -42,15 +44,20 @@ init githubToken =
 -- GITHUB REQUESTS
 
 
-fetch :: Token -> String -> IO (Either String BS.ByteString)
+fetch :: Token -> String -> IO (Either String LBS.ByteString)
 fetch (Token manager token) path =
   let
     recover e =
       return $ Left $ show (e :: SomeException)
 
     attempt =
-      do  request <- Http.parseUrlThrow $ "https://api.github.com" ++ path ++ "?access_token=" ++ token
-          response <- Http.httpLbs request manager
+      do  request <- Http.parseUrlThrow $ "https://api.github.com" ++ path
+          response <- flip Http.httpLbs manager $ request
+            { Http.requestHeaders =
+                [ (hAuthorization, BS.pack ("token " ++ token))
+                , (hUserAgent, "package.elm-lang.org")
+                ]
+            }
           return $ Right $ Http.responseBody response
   in
     attempt `catch` recover
