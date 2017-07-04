88e47f413bb7ef0e3fc3a7c36ff13b63b3407c7c
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-03 21:28:50 -0700

    Get compiling with "move transaction"

--- migration/Migrate.hs
+++ migration/Migrate.hs
@@ -2,13 +2,18 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 module Main where
 
+import Control.Monad (forM_)
 import Control.Monad.Trans (liftIO)
 import System.Console.CmdArgs
+import qualified System.Directory as Dir
 
 import qualified Crawl
+import qualified Elm.Package as Pkg
 import qualified GetDates
 import qualified Task
-import qualified MoveElmJson
+import qualified MoveDocs as Docs
+import qualified MoveElmJson as ElmJson
+import qualified MoveReadme as Readme
 
 
 
@@ -18,7 +23,8 @@ import qualified MoveElmJson
 
 data Flags =
   Flags
-    { github :: String
+    { batch :: Int
+    , github :: String
     }
     deriving (Data,Typeable,Show,Eq)
 
@@ -26,7 +32,9 @@ data Flags =
 flags :: Flags
 flags =
   Flags
-    { github = ""
+    { batch = 10
+        &= help "Maximum number of packages to process"
+    , github = ""
         &= help "OAuth token for talking to GitHub"
     }
 
@@ -39,8 +47,17 @@ main :: IO ()
 main =
   do  cargs <- cmdArgs flags
       Task.run (github cargs) $
-        do  newPackages <- Crawl.newPackages
-
-            MoveElmJson.move newPackages
-
+        do  newPackages <- take (batch cargs) <$> Crawl.newPackages
+            mapM_ moveAssets newPackages
             GetDates.check newPackages
+
+
+moveAssets :: Crawl.Package -> Task.Task ()
+moveAssets (Crawl.Package pkg vsns) =
+  forM_ vsns $ \vsn ->
+    Task.attempt pkg vsn $
+      do  liftIO $ putStrLn $ Pkg.toString pkg ++ " " ++ Pkg.versionToString vsn
+          liftIO $ Dir.createDirectoryIfMissing True (Crawl.newDir pkg vsn)
+          Readme.move pkg vsn
+          ElmJson.move pkg vsn
+          Docs.move pkg vsn
