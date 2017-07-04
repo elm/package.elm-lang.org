eeb33d700512e97fa9645bad896241d3b2d59ae3
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-03 17:28:02 -0700

    Update UpdateJson to use tasks, crawl, etc.

--- migration/UpdateJson.hs
+++ migration/UpdateJson.hs
@@ -1,92 +1,74 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE OverloadedStrings #-}
-module Main where
+module UpdateJson (update) where
 
-import Control.Monad
+import Control.Monad.Except (throwError)
+import Control.Monad.Trans (liftIO)
 import Data.Aeson ((.:))
-import Data.Map (Map, (!))
-import Data.Set (Set)
-import Data.Text (Text)
-
 import qualified Data.Aeson as Json
 import qualified Data.ByteString.Lazy as BS
 import qualified Data.HashMap.Lazy as HashMap
-import qualified Data.List as List
 import qualified Data.Map as Map
-import qualified Data.Set as Set
 import qualified Data.Text as Text
-
-import qualified System.Directory as Dir
-import System.Exit (exitFailure)
 import System.FilePath ((</>))
-import System.IO (hPutStrLn, stderr)
+
+import qualified Crawl
+import qualified Elm.Package as Pkg
+import qualified Task
 
 
 
 -- MAIN
 
 
-main :: IO ()
-main =
-  do  set <- gatherLicenses
-      print set
+update :: [Crawl.Package] -> Task.Task ()
+update packages =
+  mapM_ updatePackage packages
 
 
-gatherLicenses :: IO (Set Text)
-gatherLicenses =
-  do  users <- getSubDirs "packages"
-      Set.unions <$> mapM gatherProjects users
+updatePackage :: Crawl.Package -> Task.Task ()
+updatePackage (Crawl.Package pkg vsns) =
+  mapM_ (updateVersion pkg) vsns
 
 
-gatherProjects :: String -> IO (Set Text)
-gatherProjects user =
-  do  projects <- getSubDirs ("packages" </> user)
-      Set.unions <$> mapM (gatherVersions user) projects
+updateVersion :: Pkg.Name -> Pkg.Version -> Task.Task ()
+updateVersion pkg version =
+  do  let oldJson = Crawl.oldDir pkg version </> "elm-package.json"
+      content <- liftIO $ BS.readFile oldJson
+      case Json.eitherDecode content of
+        Left err ->
+          throwError $ "Problem at " ++ oldJson ++ "\n" ++ err
+
+        Right (Project object) ->
+          do  let newJson = Crawl.newDir pkg version </> "elm.json"
+              liftIO $ BS.writeFile newJson (Json.encode object)
 
 
-gatherVersions :: String -> String -> IO (Set Text)
-gatherVersions user project =
-  do  versions <- getSubDirs ("packages" </> user </> project)
-      Set.fromList <$> mapM (readLicense user project) versions
-
-
-readLicense :: String -> String -> String -> IO Text
-readLicense user project version =
-  do  let dir = "packages" </> user </> project </> version
-      json <- BS.readFile (dir </> "elm-package.json")
-      case Json.decode json of
-        Nothing ->
-          do  hPutStrLn stderr ("Problem at " ++ dir)
-              exitFailure
-
-        Just (Project oldLicense obj) ->
-          do  BS.writeFile (dir </> "elm.json") (Json.encode obj)
-              return oldLicense
-
-
-data Project = Project Text Json.Object
+data Project = Project Json.Object
 
 
 instance Json.FromJSON Project where
   parseJSON =
-    Json.withObject "Project" $ \obj ->
+    Json.withObject "Project" $ \object ->
       do
-          (Json.String license) <- obj .: "license"
-          let spdx = conversions ! license
+          (Json.String license) <- object .: "license"
+          (Json.String repo) <- object .: "repository"
 
-          (Json.String repo) <- obj .: "repository"
-          let name = toName repo
+          case Map.lookup license conversions of
+            Nothing ->
+              fail $ "cannot find " ++ Text.unpack license ++ " in license conversions table"
 
-          return $ Project license $
-            HashMap.insert "type" (Json.String "package") $
-            HashMap.delete "repository" $
-            HashMap.insert "name" (Json.String name) $
-            HashMap.insert "license" (Json.String spdx) $
-            HashMap.insert "test-dependencies" (Json.Object HashMap.empty) $
-              obj
+            Just spdxLicense ->
+              return $ Project $
+                HashMap.insert "type" (Json.String "package") $
+                HashMap.delete "repository" $
+                HashMap.insert "name" (Json.String (toName repo)) $
+                HashMap.insert "license" (Json.String spdxLicense) $
+                HashMap.insert "test-dependencies" (Json.Object HashMap.empty) $
+                  object
 
 
-toName :: Text -> Text
+toName :: Text.Text -> Text.Text
 toName repository =
   Text.dropEnd 4 $
   Text.replace "http://github.com/" "" $
@@ -98,13 +80,13 @@ toName repository =
 -- LICENSE CONVERTER
 
 
-conversions :: Map Text Text
+conversions :: Map.Map Text.Text Text.Text
 conversions =
   Map.fromList
-    [ "" ==> "BSD-3-Clause"
+--    [ "" ==> "BSD-3-Clause"
 
     -- BSD
-    , "BSD3" ==> "BSD-3-Clause"
+    [ "BSD3" ==> "BSD-3-Clause"
     , "BSD3-Clause" ==> "BSD-3-Clause"
     , "BSD 3-Clause" ==> "BSD-3-Clause"
     , "BSD-3-Clause" ==> "BSD-3-Clause"
@@ -143,21 +125,3 @@ conversions =
 (==>) =
   (,)
 
-
-
--- DIRECTORIES
-
-
-getSubDirs :: FilePath -> IO [FilePath]
-getSubDirs dir =
-  do  contents <- Dir.getDirectoryContents dir
-      reverse <$> foldM (addSubDir dir) [] contents
-
-
-addSubDir :: FilePath -> [FilePath] -> FilePath -> IO [FilePath]
-addSubDir dir subs subDir =
-  do  let path = dir </> subDir
-      exists <- Dir.doesDirectoryExist path
-      if exists && not (List.isPrefixOf "." subDir)
-        then return (subDir : subs)
-        else return subs
