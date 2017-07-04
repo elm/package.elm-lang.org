f38a7a70c7efde2c28acc3df429773a6902f760d
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-03 21:28:16 -0700

    Add logic for migrating docs.json
    Trying to get rid of some old inconsistencies

---
+++ migration/MoveDocs.hs
@@ -0,0 +1,59 @@
+{-# OPTIONS_GHC -Wall #-}
+module MoveDocs (move) where
+
+import Control.Monad.Trans (liftIO)
+import qualified Data.ByteString.Lazy as BS
+import qualified Data.Map as Map
+import qualified Data.Set as Set
+import qualified Data.Text as Text
+import System.FilePath ((</>))
+
+import qualified Crawl
+import qualified Elm.Docs as Docs
+import qualified Elm.Package as Pkg
+import qualified Task
+import qualified Json.Decode as Decode
+import qualified Json.Encode as Encode
+
+
+
+-- MOVE
+
+
+move :: Pkg.Name -> Pkg.Version -> Task.Transaction ()
+move pkg version =
+  do  content <- liftIO $ BS.readFile (Crawl.oldDir pkg version </> "documentation.json")
+      case Decode.parse (Decode.list Docs.decoder) content of
+        Left _ ->
+          Task.bail "Problem parsing documentation.json"
+
+        Right oldDocs ->
+          liftIO $
+          do  newDocs <- traverse fixUnmarkedInfix oldDocs
+              Encode.write (Crawl.newDir pkg version </> "docs.json") $
+                Encode.list Docs.encode newDocs
+
+
+fixUnmarkedInfix :: Docs.Module -> IO Docs.Module
+fixUnmarkedInfix (Docs.Module moduleName docs) =
+  let
+    fixEntry name entry@(Docs.Entry comment value) =
+      case value of
+        Docs.Value tipe ->
+          if Text.any (\c -> Set.member c symbols) name then
+            do  putStrLn $ "  - (" ++ Text.unpack name ++ ") had no assoc/prec"
+                return $ Docs.Entry comment (Docs.Infix tipe Docs.Left 9)
+
+          else
+            return entry
+
+        Docs.Infix _ _ _ ->
+          return entry
+  in
+    do  values <- Map.traverseWithKey fixEntry (Docs._values docs)
+        return $ Docs.Module moduleName (docs {Docs._values = values })
+
+
+symbols :: Set.Set Char
+symbols =
+  Set.fromList "!@#$%^&*+-|<>.:"
