3a5666e6e4e7e15ce28e86b5efb36be5441e4aeb
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-03 17:27:27 -0700

    Start maintaining "old" and "new" directories

--- migration/Crawl.hs
+++ migration/Crawl.hs
@@ -2,18 +2,24 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Crawl
   ( Package(..)
-  , crawl
+  , newPackages
+  , oldDir
+  , newDir
   )
   where
 
 import Control.Monad (foldM, forM)
+import Control.Monad.Except (throwError)
+import Control.Monad.Trans (liftIO)
 import qualified Data.List as List
+import qualified Data.Map as Map
 import Data.Monoid ((<>))
 import qualified Data.Text as Text
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
 
 import qualified Elm.Package as Pkg
+import qualified Task
 
 
 
@@ -27,22 +33,63 @@ data Package =
     }
 
 
+oldDir :: Pkg.Name -> Pkg.Version -> FilePath
+oldDir pkg vsn =
+  "packages-18" </> Pkg.toFilePath pkg </> Pkg.versionToString vsn
+
+
+newDir :: Pkg.Name -> Pkg.Version -> FilePath
+newDir pkg vsn =
+  "packages" </> Pkg.toFilePath pkg </> Pkg.versionToString vsn
+
+
+
+-- NEW PACKAGES
+
+
+newPackages :: Task.Task [Package]
+newPackages =
+  do  new <- crawl "packages"
+      old <- crawl "packages-18"
+      return (diff new old)
+
+
+diff :: [Package] -> [Package] -> [Package]
+diff new old =
+  let
+    destruct (Package pkg vsns) =
+      (pkg, vsns)
+
+    newDict =
+      Map.fromList (map destruct new)
+
+    isUnknown (Package pkg vsns) =
+      case Map.lookup pkg newDict of
+        Nothing ->
+          True
+
+        Just newVersions ->
+          vsns /= newVersions
+  in
+    filter isUnknown old
+
+
 
 -- CRAWL
 
 
-crawl :: FilePath -> IO [Package]
+crawl :: FilePath -> Task.Task [Package]
 crawl root =
   do  userNames <- getSubDirs root
       concat <$> traverse (crawlUser root) userNames
 
 
-crawlUser :: FilePath -> String -> IO [Package]
+crawlUser :: FilePath -> String -> Task.Task [Package]
 crawlUser root user =
   do  projectNames <- getSubDirs (root </> user)
       forM projectNames $ \project ->
         do  vsns <- getSubDirs (root </> user </> project)
-            either error return (toPackage user project vsns)
+            either throwError return (toPackage user project vsns)
 
 
 toPackage :: String -> String -> [String] -> Either String Package
@@ -56,8 +103,9 @@ toPackage user project versions =
 -- HELPERS
 
 
-getSubDirs :: FilePath -> IO [FilePath]
+getSubDirs :: FilePath -> Task.Task [FilePath]
 getSubDirs dir =
+  liftIO $
   do  contents <- Dir.getDirectoryContents dir
       reverse <$> foldM (addSubDir dir) [] contents
 
