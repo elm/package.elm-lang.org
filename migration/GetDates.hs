5aa77d4c8b6e216109ecdbc027a10a8653e82133
Evan Czaplicki <evancz@users.noreply.github.com>
2017-07-05 15:02:22 -0700

    Handle many special cases that need extra care

--- migration/GetDates.hs
+++ migration/GetDates.hs
@@ -5,9 +5,11 @@ module GetDates (get) where
 import Control.Monad.Except (catchError, throwError)
 import Control.Monad.Trans (liftIO)
 import qualified Data.List as List
+import qualified Data.Map as Map
 import qualified Data.Text as Text
-import qualified Data.Time.ISO8601 as Time
+import qualified Data.Time.Clock as Time
 import qualified Data.Time.Clock.POSIX as Time
+import qualified Data.Time.ISO8601 as Time
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
 
@@ -54,21 +56,25 @@ getRelease pkg vsn =
         if exists then
           do  string <- liftIO $ readFile timeFile
               return (Just (fromIntegral (read string :: Integer)))
-
         else
-          do  endpoint <- getEndpoint pkg vsn
-              time <- getTime pkg endpoint
-              liftIO $ putStrLn $ Pkg.toString pkg ++ " " ++ Pkg.versionToString vsn
-              liftIO $ writeFile timeFile (show (floor time :: Integer))
-              return (Just time)
-          `catchError` \err ->
-            if List.isInfixOf "statusCode = 403" err then
-              throwError $ "probably ran out of requests...\n\n" ++ err
-            else
-              do  liftIO $ putStrLn $ Pkg.toString pkg ++ " " ++ Pkg.versionToString vsn ++ " download failed"
-                  return Nothing
+          maybe (getReleaseHelp pkg vsn timeFile) (return . Just) (checkHandCollected pkg vsn)
 
       return (Releases.Release vsn <$> maybeTime)
+
+
+getReleaseHelp :: Pkg.Name -> Pkg.Version -> FilePath -> Task.Task (Maybe Time.POSIXTime)
+getReleaseHelp pkg vsn timeFile =
+  do  endpoint <- getEndpoint pkg vsn
+      time <- getTime pkg endpoint
+      liftIO $ putStrLn $ Pkg.toString pkg ++ " " ++ Pkg.versionToString vsn
+      liftIO $ writeFile timeFile (show (floor time :: Integer))
+      return (Just time)
+  `catchError` \err ->
+    if List.isInfixOf "statusCode = 403" err then
+      throwError $ "probably ran out of requests...\n\n" ++ err
+    else
+      do  liftIO $ putStrLn $ Pkg.toString pkg ++ " " ++ Pkg.versionToString vsn ++ " download failed"
+          return Nothing
 
 
 
@@ -128,4 +134,41 @@ timeDecoder person =
           fail "Not a valid ISO 8601 date."
 
         Just utcTime ->
-          return (1000 * Time.utcTimeToPOSIXSeconds utcTime)
+          return (toPosix utcTime)
+
+
+toPosix :: Time.UTCTime -> Time.POSIXTime
+toPosix utcTime =
+  1000 * Time.utcTimeToPOSIXSeconds utcTime
+
+
+
+-- HAND-COLLECTED TIMES
+
+
+checkHandCollected :: Pkg.Name -> Pkg.Version -> Maybe Time.POSIXTime
+checkHandCollected pkg vsn =
+  let name = Pkg.toString pkg ++ "@" ++ Pkg.versionToString vsn
+  in
+    toPosix <$> (Time.parseISO8601 =<< Map.lookup name handCollectedTimes)
+
+
+handCollectedTimes :: Map.Map String String
+handCollectedTimes =
+  Map.fromList
+    [ "amilner42/keyboard-extra@1.0.0" ==> "2017-02-18T03:13:59Z"
+    , "elm-community/typed-svg@1.0.0" ==> "2017-04-28T09:33:37Z"
+    , "Janiczek/elm-markov@2.0.1" ==> "2016-05-19T19:15:09Z"
+    , "krisajenkins/elm-exts@3.2.0" ==> "2015-03-24T23:18:13Z"
+    , "krisajenkins/elm-exts@4.0.0" ==> "2015-04-25T21:29:31Z"
+    , "lynn/elm-arithmetic@2.0.1" ==> "2016-07-13T19:52:52Z"
+    , "patrickjtoy/elm-table@2.0.1" ==> "2017-05-19T15:55:43Z"
+    , "tilmans/elm-style-elements-drag-drop@1.0.1" ==> "2017-07-02T16:35:24Z"
+    , "tilmans/elm-style-elements-drag-drop@1.0.2" ==> "2017-07-02T16:35:24Z"
+    , "w0rm/elm-slice-show@3.0.1" ==> "2016-06-04T15:13:24Z"
+    , "w0rm/elm-slice-show@3.0.0" ==> "2016-06-04T14:53:36Z"
+    ]
+
+
+(==>) :: a -> b -> ( a, b )
+(==>) = (,)
--- migration/Migrate.hs
+++ migration/Migrate.hs
@@ -1,9 +1,11 @@
 {-# OPTIONS_GHC -Wall #-}
 {-# LANGUAGE DeriveDataTypeable #-}
+{-# LANGUAGE OverloadedStrings #-}
 module Main where
 
 import Control.Monad (forM_)
 import Control.Monad.Trans (liftIO)
+import qualified Data.Set as Set
 import System.Console.CmdArgs
 import qualified System.Directory as Dir
 
@@ -32,7 +34,7 @@ data Flags =
 flags :: Flags
 flags =
   Flags
-    { batch = 10
+    { batch = 40
         &= help "Maximum number of packages to process"
     , github = ""
         &= help "OAuth token for talking to GitHub"
@@ -48,7 +50,7 @@ main =
   do  cargs <- cmdArgs flags
       Task.run (github cargs) $
         do  liftIO $ putStrLn "---- CRAWLING DIRECTORIES ----"
-            newPackages <- take (batch cargs) <$> Crawl.newPackages
+            newPackages <- skipProblems <$> Crawl.newPackages
 
             liftIO $ putStrLn "---- MOVING ASSETS ----"
             mapM_ moveAssets newPackages
@@ -56,8 +58,7 @@ main =
             liftIO $ putStrLn "---- CHECKING RELEASE DATES ----"
             Dates.get =<< Crawl.upgradingPackages
 
-            liftIO $ putStrLn "---- CHECKING ENDPOINTS ----"
-            liftIO $ putStrLn "this is not implemented yet"
+            liftIO $ putStrLn "DONE"
 
 
 moveAssets :: Crawl.Package -> Task.Task ()
@@ -69,3 +70,45 @@ moveAssets (Crawl.Package pkg vsns) =
           Readme.move pkg vsn
           ElmJson.move pkg vsn
           Docs.move pkg vsn
+
+
+
+-- PROBLEM PACKAGES
+
+
+skipProblems :: [Crawl.Package] -> [Crawl.Package]
+skipProblems packages =
+  filter (\(Crawl.Package pkg _) -> not (Set.member pkg problemPackages)) packages
+
+
+problemPackages :: Set.Set Pkg.Name
+problemPackages =
+  Set.fromList
+    [ Pkg.Name "joneshf" "elm-proof"                -- crazy module name in docs
+    , Pkg.Name "adam-r-kowalski" "elm-css-legacy"   -- deleted
+    , Pkg.Name "danstn" "elm-postgrest"             -- deleted
+    , Pkg.Name "elm-community" "elm-function-extra" -- deleted
+    , Pkg.Name "elm-community" "elm-undo-redo"      -- deleted
+    , Pkg.Name "folkertdev" "elm-ordereddict"       -- deleted
+    , Pkg.Name "frenchdonuts" "elm-autocomplete"    -- too weird
+    , Pkg.Name "geekyme" "elm-charts"               -- deleted
+    , Pkg.Name "humio" "elm-plot"                   -- missing tags / why fork?
+    , Pkg.Name "izdi" "junk"                        -- deleted
+    , Pkg.Name "JasonGFitzpatrick" "elm-router"     -- deleted
+    , Pkg.Name "jasonmahr" "html-escaped-unicode"   -- deleted
+    , Pkg.Name "JOEANDAVERDE" "flex-html"           -- deleted
+    , Pkg.Name "joonazan" "elm-ast"                 -- missing tags / why fork?
+    , Pkg.Name "jvoigtlaender" "elm-drag-and-drop"  -- deleted
+    , Pkg.Name "lagunoff" "elm-mdl"                 -- deleted
+    , Pkg.Name "lukewestby-fake-elm-lang-1" "redirect-test-1" -- deleted
+    , Pkg.Name "nicklawls" "elm-html-animation"     -- deleted
+    , Pkg.Name "nvaldes" "elm-bootstrap"            -- deprecated
+    , Pkg.Name "omarroth" "elm-dom"                 -- deleted
+    , Pkg.Name "omarroth" "elm-parts"               -- deleted
+    , Pkg.Name "rluiten" "lunrelm"                  -- deprecated
+    , Pkg.Name "RomanErnst" "updated-list"          -- deleted
+    , Pkg.Name "Spottt" "elm-dialog"                -- missing tags / why fork?
+    , Pkg.Name "terezka" "elm-view-utils"           -- renamed
+    , Pkg.Name "wittjosiah" "elm-css"               -- deleted
+    , Pkg.Name "z5h" "time-app"                     -- deprecated
+    ]
