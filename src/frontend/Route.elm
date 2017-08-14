80abc1c19a60eb564c91c70b9edaf06d77d8e783
Evan Czaplicki <evancz@users.noreply.github.com>
2017-08-13 22:56:48 -0700

    Use new Url.Builder and Url.Parser stuff

--- src/frontend/Route.elm
+++ src/frontend/Route.elm
@@ -1,12 +1,15 @@
 module Route exposing
   ( Route(..)
   , Version(..)
+  , VersionInfo(..)
   , fromLocation
   , toString
+  , vsnToString
   )
 
 
-import UrlParser exposing (Parser, (</>), map, s)
+import Url.Builder as B
+import Url.Parser as Url exposing (Parser, (</>), map, s, fragment)
 import Version
 
 
@@ -18,8 +21,7 @@ type Route
   = Home
   | User String
   | Package String String
-  | Readme String String Version
-  | Module String String Version String (Maybe String)
+  | Version String String Version VersionInfo
   | Guidelines
   | DocsHelp
 
@@ -29,31 +31,30 @@ type Version
   | Exactly Version.Version
 
 
+type VersionInfo
+  = Readme
+  | Module String (Maybe String)
+
+
 
 -- LOCATION TO ROUTE
 
 
 fromLocation : Location -> Maybe Route
 fromLocation location =
-  case UrlParser.parsePath location of
-    Just (Module user project vsn moduleName _) as result ->
-      if String.isEmpty location.hash then
-        result
-      else
-        Just (Module user project vsn moduleName (Just (String.dropLeft 1 location.hash)))
-
-    result ->
-      result
+  Url.parseLocation parser
+    (Url.preparePath location.pathname)
+    (Url.prepareQuery location.search)
+    (Url.prepareFragment location.hash)
 
 
 parser : Parser (Route -> a) a
 parser =
-  UrlParser.oneOf
-    [ map Home        <| top
+  Url.oneOf
+    [ map Home        <| Url.top
     , map User        <| s "packages" </> user
     , map Package     <| s "packages" </> user </> project
-    , map Readme      <| s "packages" </> user </> project </> version
-    , map Module      <| s "packages" </> user </> project </> version </> moduleName </> map Nothing top
+    , map Version     <| s "packages" </> user </> project </> version </> versionInfo
     , map Guidelines  <| s "help" </> s "design-guidelines"
     , map DocsHelp    <| s "help" </> s "docs"
     ]
@@ -61,26 +62,34 @@ parser =
 
 user : Parser (String -> a) a
 user =
-  UrlParser.custom "USER" Ok
+  Url.custom "USER" Just
 
 
 project : Parser (String -> a) a
 project =
-  UrlParser.custom "PROJECT" Ok
+  Url.custom "PROJECT" Just
 
 
 version : Parser (Version -> a) a
 version =
-  UrlParser.custom "VERSION" <| \string ->
+  Url.custom "VERSION" <| \string ->
     if string == "latest" then
       Just Latest
     else
       Maybe.map Exactly (Version.fromString string)
 
 
+versionInfo : Parser (VersionInfo -> a) a
+versionInfo =
+  Url.oneOf
+    [ map Readme Url.top
+    , map Module (moduleName </> fragment)
+    ]
+
+
 moduleName : Parser (String -> a) a
 moduleName =
-  UrlParser.custom "MODULE" (Ok << String.replace "-" ".")
+  Url.custom "MODULE" (Just << String.replace "-" ".")
 
 
 
@@ -91,31 +100,27 @@ toString : Route -> String
 toString route =
   case route of
     Home ->
-      "/"
+      B.absolute [] []
 
     User user ->
-      "/packages/" ++ user ++ "/"
+      B.absolute [ "packages", user ] []
 
     Package user project ->
-      "/packages/" ++ user ++ "/" ++ project ++ "/"
+      B.absolute [ "packages", user, project ] []
 
-    Readme user project vsn ->
-      "/packages/" ++ user ++ "/" ++ project ++ "/" ++ vsnToString vsn ++ "/"
+    Version user project vsn Readme ->
+      B.absolute [ "packages", user, project, vsnToString vsn ] []
 
-    Module user project vsn moduleName maybeValue ->
-      let
-        path =
-          "/packages/" ++ user
-          ++ "/" ++ project
-          ++ "/" ++ vsnToString vsn
-          ++ "/" ++ String.replace "." "-" moduleName
-      in
-        case maybeValue of
-          Nothing ->
-            path
-
-          Just value ->
-            path ++ "#" ++ value
+    Version user project vsn (Module moduleName maybeValue) ->
+      B.custom B.Absolute
+        [ "packages"
+        , user
+        , project
+        , vsnToString vsn
+        , String.replace "." "-" moduleName
+        ]
+        []
+        maybeValue
 
     Guidelines ->
       "/help/design-guidelines"
