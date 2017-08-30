b3c9234f8f8a5bedf88fbbe1b107699bb749588e
Evan Czaplicki <evancz@users.noreply.github.com>
2017-08-30 03:01:03 -0700

    Get a body structure to use for all major content

--- src/frontend/Utils/App.elm
+++ src/frontend/Utils/App.elm
@@ -1,12 +1,31 @@
 module Utils.App exposing
-  ( link
+  ( Body, body, viewBody
+  , link
   )
 
-import Html exposing (Html, Attribute, a)
-import Html.Attributes exposing (href)
+import Html exposing (Html, Attribute, a, div)
+import Html.Attributes exposing (class, href)
 import Html.Events exposing (preventDefaultOn)
 import Json.Decode as D
 import Route exposing (Route)
+
+
+
+-- NODE
+
+
+type Body msg =
+  Body (List (Attribute msg)) (List (Html msg))
+
+
+body : List (Attribute msg) -> List (Html msg) -> Body msg
+body =
+  Body
+
+
+viewBody : Body msg -> Html msg
+viewBody (Body attrs kids) =
+  div (class "center" :: attrs) kids
 
 
 
