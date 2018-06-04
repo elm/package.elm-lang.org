9100d7c19f31774076fc56c92b535dc07e606cf0
Evan Czaplicki <evancz@users.noreply.github.com>
2018-06-04 09:28:34 -0700

    Remove Google Analytics

--- src/backend/ServeFile.hs
+++ src/backend/ServeFile.hs
@@ -106,7 +106,6 @@ makeHtml title canonialLink =
       H.meta ! A.charset "UTF-8"
       favicon
       H.title (H.toHtml title)
-      googleAnalytics
       canonialLink
       H.link ! A.rel "stylesheet" ! A.href (cacheBuster "/assets/highlight/styles/default.css")
       H.link ! A.rel "stylesheet" ! A.href (cacheBuster "/assets/style.css")
@@ -114,16 +113,6 @@ makeHtml title canonialLink =
       H.script ! A.src (cacheBuster Artifacts.js) $ ""
 
     H.body $ H.script $ H.preEscapedToMarkup ("\nElm.Main.fullscreen()\n" :: String)
-
-
-googleAnalytics :: H.Html
-googleAnalytics =
-  H.script ! A.type_ "text/javascript" $
-    "(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){\
-    \(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),\
-    \m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)\
-    \})(window,document,'script','//www.google-analytics.com/analytics.js','ga');\
-    \ga('create','UA-25827182-1','auto');ga('send','pageview');"
 
 
 favicon :: H.Html
