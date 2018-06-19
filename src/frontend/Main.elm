7a128a97130081d9ad659a4da968064001fbe1f3
Evan Czaplicki <evancz@users.noreply.github.com>
2018-06-19 03:24:03 -0500

    No need for a special Route type anymore

--- src/frontend/Main.elm
+++ src/frontend/Main.elm
@@ -2,24 +2,17 @@ module Main exposing (..)
 
 
 import Browser
-import Browser.Navigation as Navigation
-import Html exposing (..)
-import Html.Attributes exposing (..)
-import Html.Lazy exposing (..)
+import Browser.Navigation as Nav
+import Dict
+import Elm.Version as V
+import Html
 import Page.Docs as Docs
 import Page.Diff as Diff
-import Page.Problem as Problem
 import Page.Search as Search
-import Process
-import Route
 import Session
-import Session.Query as Query
-import Session.Status exposing (Status(..))
-import Task
+import Skeleton
 import Url
-import Utils.App as App
-import Utils.OneOrMore as OneOrMore
-import Version
+import Url.Parser as Parser exposing (Parser, (</>), custom, fragment, map, oneOf, s, top)
 
 
 
@@ -27,12 +20,13 @@ import Version
 
 
 main =
-  Browser.fullscreen
+  Browser.application
     { init = init
     , view = view
     , update = update
     , subscriptions = subscriptions
-    , onNavigation = Just onNavigation
+    , onUrlRequest = LinkClicked
+    , onUrlChange = UrlChanged
     }
 
 
@@ -41,27 +35,16 @@ main =
 
 
 type alias Model =
-  { session : Session.Data
-  , route : Route.Route
+  { key : Nav.Key
   , page : Page
   }
 
 
 type Page
-  = Blank
-  | Problem Problem.Suggestion
-  | Diff Diff.Model
-  | Docs Docs.Model
+  = Blank Session.Data
   | Search Search.Model
-
-
-
--- INIT
-
-
-init : Browser.Env () -> ( Model, Cmd Msg )
-init env =
-  check (Model Session.empty (Route.fromUrl env.url) Blank)
+  | Docs Docs.Model
+  | Diff Diff.Model
 
 
 
@@ -74,12 +57,37 @@ subscriptions model =
 
 
 
--- NAVIGATION
+-- VIEW
 
 
-onNavigation : Url.Url -> Msg
-onNavigation url =
-  ReactToUrl (Route.fromUrl url)
+view : Model -> Browser.Document Msg
+view model =
+  case model.page of
+    Blank _ ->
+      { title = "???" -- TODO
+      , body = [ Html.text "hello" ]
+      }
+
+    Search search ->
+      Skeleton.view SearchMsg (Search.view search)
+
+    Docs docs ->
+      Skeleton.view DocsMsg (Docs.view docs)
+
+    Diff diff ->
+      Skeleton.view never (Diff.view diff)
+
+
+
+-- INIT
+
+
+init : () -> Url.Url -> Nav.Key -> ( Model, Cmd Msg )
+init _ url key =
+  stepUrl url
+    { key = key
+    , page = Blank Session.empty
+    }
 
 
 
@@ -88,11 +96,12 @@ onNavigation url =
 
 type Msg
   = NoOp
-  | PushUrl Route.Route
-  | ReactToUrl Route.Route
-  | SessionMsg Session.Msg
-  | DocsMsg Docs.Msg
+  | LinkClicked Browser.UrlRequest
+  | UrlChanged Url.Url
   | SearchMsg Search.Msg
+  | DiffMsg Diff.Msg
+  | DocsMsg Docs.Msg
+
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -101,419 +110,136 @@ update message model =
     NoOp ->
       ( model, Cmd.none )
 
-    PushUrl route ->
-      ( model
-      , Navigation.pushUrl (Route.toUrl route)
-      )
+    LinkClicked urlRequest ->
+      case urlRequest of
+        Browser.Internal url ->
+          ( model
+          , Nav.pushUrl model.key (Url.toString url)
+          )
 
-    ReactToUrl route ->
-      check { model | route = route }
+        Browser.External href ->
+          ( model
+          , Nav.load href
+          )
 
-    SessionMsg msg ->
-      check { model | session = Session.update msg model.session }
-
-    DocsMsg msg ->
-      case model.page of
-        Docs docsModel ->
-          let
-            (newDocsModel, cmds) =
-               Docs.update msg docsModel
-          in
-          ( { model | page = Docs newDocsModel }, cmds )
-
-        _ ->
-          ( model, Cmd.none )
+    UrlChanged url ->
+      stepUrl url model
 
     SearchMsg msg ->
       case model.page of
-        Search searchModel ->
-          let
-            (newSearchModel, cmds) =
-               Search.update msg searchModel
-          in
-          ( { model | page = Search newSearchModel }, cmds )
+        Search search -> stepSearch model (Search.update msg search)
+        _             -> ( model, Cmd.none )
 
-        _ ->
-          ( model, Cmd.none )
+    DiffMsg msg ->
+      case model.page of
+        Diff diff -> stepDiff model (Diff.update msg diff)
+        _         -> ( model, Cmd.none )
+
+    DocsMsg msg ->
+      case model.page of
+        Docs docs -> stepDocs model (Docs.update msg docs)
+        _         -> ( model, Cmd.none )
+
+
+stepSearch : Model -> ( Search.Model, Cmd Search.Msg ) -> ( Model, Cmd Msg )
+stepSearch model (search, cmds) =
+  ( { model | page = Search search }
+  , Cmd.map SearchMsg cmds
+  )
+
+
+stepDocs : Model -> ( Docs.Model, Cmd Docs.Msg ) -> ( Model, Cmd Msg )
+stepDocs model (search, cmds) =
+  ( { model | page = Docs search }
+  , Cmd.map DocsMsg cmds
+  )
+
+
+stepDiff : Model -> ( Diff.Model, Cmd Diff.Msg ) -> ( Model, Cmd Msg )
+stepDiff model (search, cmds) =
+  ( { model | page = Diff search }
+  , Cmd.map DiffMsg cmds
+  )
 
 
 
--- ROUTING
+-- EXIT
 
 
-check : Model -> ( Model, Cmd Msg )
-check model =
+exit : Model -> Session.Data
+exit model =
+  case model.page of
+    Blank session -> session
+    Search m -> m.session
+    Docs m -> m.session
+    Diff m -> m.session
+
+
+
+-- ROUTER
+
+
+stepUrl : Url.Url -> Model -> (Model, Cmd Msg)
+stepUrl url model =
   let
-    goto page =
-      ( { model | page = page }, Cmd.none )
+    session =
+      exit model
+
+    parser =
+      oneOf
+        [ route top
+            ( stepSearch model (Search.init session)
+            )
+        , route (s "packages" </> author_ </> project_)
+            (\author project ->
+                stepDiff model (Diff.init session author project)
+            )
+        , route (s "packages" </> author_ </> project_ </> version_ </> focus_)
+            (\author project version focus ->
+                stepDocs model (Docs.init session author project version focus)
+            )
+        ]
   in
-  case model.route of
-    Route.Home ->
-      onSuccess model <|
-        Query.map (Search << Search.Model "") Session.packages
+  case Parser.parse parser url of
+    Just answer ->
+      answer
 
-    Route.User name ->
-      goto Blank
-
-    Route.Package user project ->
-      let
-        toPage releases =
-          Diff (Diff.Model user project (OneOrMore.toList releases))
-      in
-      onSuccess model <|
-        Query.map toPage (Session.releases user project)
-
-    Route.Version user project version info ->
-      let
-        metadataQuery =
-          Query.map
-            (Docs.Metadata user project version info)
-            (Query.maybe (Session.latestVersion user project))
-
-        contentQuery =
-          case info of
-            Route.Readme ->
-              Query.map2 Docs.Readme (Session.readme user project version) <|
-                Query.map (Maybe.withDefault []) <|
-                  Query.maybe (Session.allDocs user project version)
-
-            Route.Module name _ ->
-              Query.map
-                (\(d,ds) -> Docs.Module name d ds)
-                (Session.moduleDocs user project version name)
-      in
-      scrollOnSuccess (Route.getHash info) model <| Query.map Docs <|
-        Query.map3 Docs.Model metadataQuery contentQuery (Query.success "")
-
-    Route.Guidelines ->
-      goto Blank
-
-    Route.DocsHelp ->
-      goto Blank
-
-    Route.NotFound url ->
-      goto (Problem Problem.NoIdea)
-
-
-onSuccess : Model -> Session.Query Page -> ( Model, Cmd Msg )
-onSuccess =
-  scrollOnSuccess Nothing
-
-
-scrollOnSuccess : Maybe String -> Model -> Session.Query Page -> ( Model, Cmd Msg )
-scrollOnSuccess maybeTag model query =
-  let
-    (newSession, cmds, status) =
-      Query.check model.session query
-
-    newPage =
-      case status of
-        Failure error _ ->
-          Problem (Problem.BadResource error)
-
-        Loading ->
-          model.page
-
-        Success page ->
-          page
-      in
-      ( { model | session = newSession, page = newPage }
-      , scrollTo maybeTag (Cmd.map SessionMsg cmds)
-      )
-
-
-scrollTo : Maybe String -> Cmd Msg -> Cmd Msg
-scrollTo maybeTag command =
-  case maybeTag of
     Nothing ->
-      command
+      (model, Cmd.none)
 
-    Just tag ->
-      Cmd.batch
-        [ Task.attempt (\_ -> NoOp) (Browser.scrollIntoView tag)
-        , command
-        ]
 
+route : Parser a b -> a -> Parser (b -> c) c
+route parser handler =
+  Parser.map handler parser
 
 
--- VIEW
+author_ : Parser (String -> a) a
+author_ =
+  custom "AUTHOR" Just
 
 
-view : Model -> Browser.Page Msg
-view model =
-  { title =
-      toTitle model.route
-  , body =
-      [ center "#eeeeee" [ lazy2 viewHeader model.session model.page ]
-      , center "#60B5CC" [ lazy2 viewVersionWarning model.session model.page ]
-      , lazy viewPage model.page
-      , viewFooter
-      ]
-  }
+project_ : Parser (String -> a) a
+project_ =
+  custom "PROJECT" Just
 
 
-center : String -> List (Html msg) -> Html msg
-center color kids =
-  div [ style "background-color" color ] [ div [class "center"] kids ]
+version_ : Parser (Maybe V.Version -> a) a
+version_ =
+  custom "VERSION" <| \string ->
+    if string == "latest" then
+      Just Nothing
+    else
+      Maybe.map Just (V.fromString string)
 
 
-toTitle : Route.Route -> String
-toTitle route =
-  case route of
-    Route.Home ->
-      "Elm Packages"
-
-    Route.User user ->
-      user
-
-    Route.Package _ project ->
-      project
-
-    Route.Version _ project version info ->
-      let
-        genericName =
-          case version of
-            Route.Latest ->
-              project
-
-            Route.Exactly vsn ->
-              project ++ " " ++ Version.toString vsn
-      in
-      case info of
-        Route.Readme ->
-          genericName
-
-        Route.Module name _ ->
-          name ++ " - " ++ genericName
-
-    Route.Guidelines ->
-      "Guidelines"
-
-    Route.DocsHelp ->
-      "Help"
-
-    Route.NotFound _ ->
-      "Not Found!"
-
-
-
--- VIEW PAGE
-
-
-viewPage : Page -> Html Msg
-viewPage page =
-  let
-    debody toMsg body =
-      Html.map toMsg (App.viewBody body)
-  in
-  case page of
-    Blank ->
-      debody identity <| App.body [] []
-
-    Problem suggestion ->
-      debody PushUrl <| Problem.view suggestion
-
-    Docs docsModel ->
-      debody DocsMsg <| Docs.view docsModel
-
-    Diff diffModel ->
-      debody PushUrl <| Diff.view diffModel
-
-    Search searchModel ->
-      debody SearchMsg <| Search.view searchModel
-
-
-
--- VIEW ROUTE LINKS
-
-
-viewHeader : Session.Data -> Page -> Html Msg
-viewHeader sessionData page =
-  h1 [ class "header" ] <| (::) homeLogo <|
-    case page of
-      Blank ->
-        []
-
-      Problem _ ->
-        []
-
-      Docs model ->
-        let
-          { user, project, version, latest, info } =
-            model.metadata
-
-          vsn =
-            getLatestVersion version latest
-        in
-        List.intersperse slash <|
-          [ text user
-          , packageLink user project
-          , versionLink user project vsn
-          ]
-          ++ moduleLink user project vsn info
-
-      Diff { user, project } ->
-        List.intersperse slash
-          [ text user
-          , packageLink user project
-          ]
-
-      Search _ ->
-        []
-
-
-packageLink : String -> String -> Html Msg
-packageLink user project =
-  toLink (Route.Package user project) project
-
-
-versionLink : String -> String -> Route.Version -> Html Msg
-versionLink user project version =
-  toLink
-    (Route.Version user project version Route.Readme)
-    (Route.vsnToString version)
-
-
-moduleLink : String -> String -> Route.Version -> Route.VersionInfo -> List (Html Msg)
-moduleLink user project version info =
-  case info of
-    Route.Readme ->
-      []
-
-    Route.Module moduleName maybeTag ->
-      let
-        route =
-          Route.Version user project version (Route.Module moduleName Nothing)
-      in
-      [ toLink route moduleName ]
-
-
-getLatestVersion : Route.Version -> Maybe Version.Version -> Route.Version
-getLatestVersion version maybeLatest =
-  case version of
-    Route.Exactly _ ->
-      version
-
-    Route.Latest ->
-      case maybeLatest of
-        Nothing ->
-          version
-
-        Just latest ->
-          Route.Exactly latest
-
-
-slash : Html msg
-slash =
-  span [ class "spacey-char" ] [ text "/" ]
-
-
-toLink : Route.Route -> String -> Html Msg
-toLink route words =
-  App.link PushUrl route [] [ text words ]
-
-
-
--- VIEW VERSION WARNINGS
-
-
-viewVersionWarning : Session.Data -> Page -> Html Msg
-viewVersionWarning session page =
-  div [ class "header-underbar" ] <|
-    case getNewerRoute session page of
-      Nothing ->
-        [ p [ class "version-warning" ]
-            [ text "NOT FOR SHARING. For alpha testers only! Learn why "
-            , a [ href "https://www.deconstructconf.com/2017/evan-czaplicki-on-storytelling" ] [ text "here" ]
-            ]
-        ]
-
-      Just (latestVersion, newerRoute) ->
-        [ p [ class "version-warning" ]
-            [ text "Warning! The latest version of this package is "
-            , App.link PushUrl newerRoute [] [ text (Version.toString latestVersion) ]
-            ]
-        ]
-
-
-getNewerRoute : Session.Data -> Page -> Maybe (Version.Version, Route.Route)
-getNewerRoute sessionData page =
-  case page of
-    Blank ->
-      Nothing
-
-    Problem _ ->
-      Nothing
-
-    Docs model ->
-      case model.metadata.version of
-        Route.Latest ->
-          Nothing
-
-        Route.Exactly version ->
-          let
-            { user, project, info, latest } =
-              model.metadata
-
-            checkLatest latestVersion =
-              if version == latestVersion then
-                Nothing
-              else
-                Just ( latestVersion, Route.Version user project Route.Latest info )
-          in
-          Maybe.andThen checkLatest latest
-
-    Diff _ ->
-      Nothing
-
-    Search _ ->
-      Nothing
-
-
-
--- VIEW FOOTER
-
-
-viewFooter : Html msg
-viewFooter =
-  div [class "footer"]
-    [ text "All code for this site is open source and written in Elm. "
-    , a [ class "grey-link", href "https://github.com/elm/package.elm-lang.org/" ] [ text "Check it out" ]
-    , text "! — © 2012-present Evan Czaplicki"
+focus_ : Parser (Docs.Focus -> a) a
+focus_ =
+  oneOf
+    [ map Docs.Readme top
+    , map Docs.Module (moduleName_ </> fragment identity)
     ]
 
 
-
--- VIEW LOGO
-
-
-homeLogo : Html Msg
-homeLogo =
-  App.link PushUrl Route.Home [ style "text-decoration" "none" ] [ logo ]
-
-
-logo : Html msg
-logo =
-  div
-    [ style "display" "-webkit-display"
-    , style "display" "-ms-flexbox"
-    , style "display" "flex"
-    ]
-    [ img
-        [ src "/assets/elm_logo.svg"
-        , style "height" "30px"
-        , style "vertical-align" "bottom"
-        , style "padding-right" "8px"
-        ]
-        []
-    , div
-        [ style "color" "black"
-        ]
-        [ div [ style "line-height" "20px" ] [ text "elm" ]
-        , div
-            [ style "line-height" "10px"
-            , style "font-size" "0.5em"
-            ]
-            [ text "packages" ]
-        ]
-    ]
+moduleName_ : Parser (String -> a) a
+moduleName_ =
+  custom "MODULE" (Just << String.replace "-" ".")
