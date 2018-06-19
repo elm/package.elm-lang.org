0ac3b8f0f09e18a4b8958505dc43125365566254
Evan Czaplicki <evancz@users.noreply.github.com>
2018-06-19 03:16:17 -0500

    Replace Utils.App with Skeleton and Href
    No longer need to do anything special for <a> elements

---
+++ src/frontend/Href.elm
@@ -0,0 +1,42 @@
+module Href exposing
+  ( toProject
+  , toVersion
+  , toModule
+  )
+
+
+import Elm.Version as V
+import Url.Builder as Url
+
+
+
+-- HREFS
+
+
+toProject : String -> String -> String
+toProject author project =
+  Url.absolute [ "packages", author, project, "" ] []
+
+
+toVersion : String -> String -> Maybe V.Version -> String
+toVersion author project version =
+  Url.absolute [ "packages", author, project, vsnToString version, ""] []
+
+
+toModule : String -> String -> Maybe V.Version -> String -> Maybe String -> String
+toModule author project version moduleName maybeValue =
+  Url.custom Url.Absolute [ "packages", author, project, vsnToString version, String.replace "." "-" moduleName ] [] maybeValue
+
+
+
+-- HELPERS
+
+
+vsnToString : Maybe V.Version -> String
+vsnToString maybeVersion =
+  case maybeVersion of
+    Nothing ->
+      "latest"
+
+    Just version ->
+      V.toString version
---
+++ src/frontend/Skeleton.elm
@@ -0,0 +1,194 @@
+module Skeleton exposing
+  ( Details
+  , Warning(..)
+  , view
+  , Segment
+  , authorSegment
+  , projectSegment
+  , versionSegment
+  , moduleSegment
+  )
+
+
+import Browser
+import Elm.Version as V
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Lazy exposing (..)
+import Href
+import Json.Decode as D
+import Utils.Logo as Logo
+
+
+
+-- NODE
+
+
+type alias Details msg =
+  { title : String
+  , header : List Segment
+  , warning : Warning
+  , attrs : List (Attribute msg)
+  , kids : List (Html msg)
+  }
+
+
+type Warning
+  = NoProblems
+  | NewerVersion String V.Version
+
+
+
+-- SEGMENT
+
+
+type Segment
+  = Text String
+  | Link String String
+
+
+authorSegment : String -> Segment
+authorSegment author =
+  Text author
+
+
+projectSegment : String -> String -> Segment
+projectSegment author project =
+  Link (Href.toProject author project) project
+
+
+versionSegment : String -> String -> Maybe V.Version -> Segment
+versionSegment author project version =
+  Link (Href.toVersion author project version) (vsnToString version)
+
+
+moduleSegment : String -> String -> Maybe V.Version -> String -> Segment
+moduleSegment author project version moduleName =
+  Link (Href.toModule author project version moduleName Nothing) moduleName
+
+
+vsnToString : Maybe V.Version -> String
+vsnToString maybeVersion =
+  case maybeVersion of
+    Nothing ->
+      "latest"
+
+    Just version ->
+      V.toString version
+
+
+
+-- VIEW
+
+
+view : (a -> msg) -> Details a -> Browser.Document msg
+view toMsg details =
+  { title =
+      details.title
+  , body =
+      [ viewHeader details.header
+      , lazy viewWarning details.warning
+      , Html.map toMsg <|
+          div (class "center" :: details.attrs) details.kids
+      , viewFooter
+      ]
+  }
+
+
+
+-- VIEW HEADER
+
+
+viewHeader : List Segment -> Html msg
+viewHeader segments =
+  div
+    [ style "background-color" "#eeeeee"
+    ]
+    [ div [class "center"]
+        [ h1 [ class "header" ] <|
+            viewLogo :: List.intersperse slash (List.map viewSegment segments)
+        ]
+    ]
+
+
+
+slash : Html msg
+slash =
+  span [ class "spacey-char" ] [ text "/" ]
+
+
+viewSegment : Segment -> Html msg
+viewSegment segment =
+  case segment of
+    Text string ->
+      text string
+
+    Link address string ->
+      a [ href address ] [ text string ]
+
+
+
+-- VIEW WARNING
+
+
+viewWarning : Warning -> Html msg
+viewWarning warning =
+  div [ class "header-underbar" ] <|
+    case warning of
+      NoProblems ->
+        [ p [ class "version-warning" ]
+            [ text "NOT FOR SHARING. For alpha testers only! Learn why "
+            , a [ href "https://www.deconstructconf.com/2017/evan-czaplicki-on-storytelling" ] [ text "here" ]
+            ]
+        ]
+
+      NewerVersion url version ->
+        [ p [ class "version-warning" ]
+            [ text "Warning! The latest version of this package is "
+            , a [ href url ] [ text (V.toString version) ]
+            ]
+        ]
+
+
+
+-- VIEW FOOTER
+
+
+viewFooter : Html msg
+viewFooter =
+  div [class "footer"]
+    [ text "All code for this site is open source and written in Elm. "
+    , a [ class "grey-link", href "https://github.com/elm/package.elm-lang.org/" ] [ text "Check it out" ]
+    , text "! — © 2012-present Evan Czaplicki"
+    ]
+
+
+
+-- VIEW LOGO
+
+
+viewLogo : Html msg
+viewLogo =
+  a [ href "/"
+    , style "text-decoration" "none"
+    ]
+    [
+      div
+        [ style "display" "-webkit-display"
+        , style "display" "-ms-flexbox"
+        , style "display" "flex"
+        ]
+        [ Logo.logo 30
+        , div
+            [ style "color" "black"
+            , style "padding-left" "8px"
+            ]
+            [ div [ style "line-height" "20px" ] [ text "elm" ]
+            , div
+                [ style "line-height" "10px"
+                , style "font-size" "0.5em"
+                ]
+                [ text "packages" ]
+            ]
+        ]
+    ]
