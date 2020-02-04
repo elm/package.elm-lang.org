0db2dff90b88e56c77af95746a174a1cf3d825d9
Evan Czaplicki <evancz@users.noreply.github.com>
2020-02-03 20:49:31 -0500

    update paths for latest elm/compiler code

--- src/backend/Package/Path.hs
+++ src/backend/Package/Path.hs
@@ -9,6 +9,7 @@ module Package.Path
 import qualified System.Directory as Dir
 import System.FilePath ((</>))
 import qualified Elm.Package as Pkg
+import qualified Elm.Version as V
 
 
 
@@ -20,16 +21,16 @@ releases pkg =
   "packages" </> Pkg.toFilePath pkg </> "releases.json"
 
 
-directory :: Pkg.Name -> Pkg.Version -> FilePath
-directory name version =
-  "packages" </> Pkg.toFilePath name </> Pkg.versionToString version
+directory :: Pkg.Name -> V.Version -> FilePath
+directory pkg vsn =
+  "packages" </> Pkg.toFilePath pkg </> V.toChars vsn
 
 
-removeDirectory :: Pkg.Name -> Pkg.Version -> IO ()
-removeDirectory name version =
+removeDirectory :: Pkg.Name -> V.Version -> IO ()
+removeDirectory pkg vsn =
   do  Dir.removeDirectoryRecursive $
-        "packages" </> Pkg.toFilePath name </> Pkg.versionToString version
+        "packages" </> Pkg.toFilePath pkg </> V.toChars vsn
 
-      if version == Pkg.initialVersion
-        then Dir.removeDirectoryRecursive ("packages" </> Pkg.toFilePath name)
+      if vsn == V.one
+        then Dir.removeDirectoryRecursive ("packages" </> Pkg.toFilePath pkg)
         else return ()
