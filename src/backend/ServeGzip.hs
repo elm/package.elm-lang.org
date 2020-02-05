f1ee5e088653f7d78583dbd976f464fcbb7bf47f
Evan Czaplicki <evancz@users.noreply.github.com>
2020-02-05 15:37:42 -0500

    add notes to encourage curl/wget users to accept gzip

--- src/backend/ServeGzip.hs
+++ src/backend/ServeGzip.hs
@@ -7,7 +7,12 @@ module ServeGzip
 
 import qualified Data.Binary.Builder as B
 import qualified Data.ByteString as BS
-import Snap.Core (Snap, finishWith, getHeader, getRequest, getResponse, sendFile, setContentType, setHeader, setResponseBody)
+import Snap.Core
+  ( Snap, finishWith, getHeader, getRequest, getResponse
+  , modifyResponse, rqURI, sendFile
+  , setContentType, setHeader, setResponseBody, setResponseStatus
+  , writeBuilder
+  )
 import qualified System.IO.Streams.Core as S
 import qualified System.IO.Streams.File as SF
 import qualified System.IO.Streams.Zlib as SZ
@@ -40,12 +45,23 @@ serveGzippedFile mimeType filePath =
                 =<< getResponse
 
         Identity ->
-          do  finishWith
-                . setHeader "Content-Encoding" "identity"
-                . setHeader "Vary" "Accept-Encoding"
-                . setContentType mimeType
-                . setResponseBody (gunzipIntoOutput filePath)
-                =<< getResponse
+          do  userAgent <- detectUserAgent
+              case userAgent of
+                Curl ->
+                  do  modifyResponse $ setResponseStatus 406 "Bad Accept-Encoding"
+                      writeBuilder . toCurlError . rqURI =<< getRequest
+
+                Wget ->
+                  do  modifyResponse $ setResponseStatus 406 "Bad Accept-Encoding"
+                      writeBuilder . toWgetError . rqURI =<< getRequest
+
+                Browser ->
+                  finishWith
+                    . setHeader "Content-Encoding" "identity"
+                    . setHeader "Vary" "Accept-Encoding"
+                    . setContentType mimeType
+                    . setResponseBody (gunzipIntoOutput filePath)
+                    =<< getResponse
 
 
 gunzipIntoOutput :: FilePath -> S.OutputStream B.Builder -> IO (S.OutputStream B.Builder)
@@ -90,3 +106,54 @@ getAcceptableEncoding =
           if BS.isInfixOf "gzip" header
           then return Gzip
           else return Identity
+
+
+
+-- DETECT USER AGENT
+
+
+data UserAgent
+  = Browser
+  | Curl
+  | Wget
+
+
+detectUserAgent :: Snap UserAgent
+detectUserAgent =
+  do  maybeHeader <- getHeader "User-Agent" <$> getRequest
+      case maybeHeader of
+        Nothing ->
+          return Browser
+
+        Just header
+          | BS.isInfixOf "curl" header -> return Curl
+          | BS.isInfixOf "Wget" header -> return Wget
+          | otherwise                  -> return Browser
+
+
+toCurlError :: BS.ByteString -> B.Builder
+toCurlError uri =
+  "-- 406 -- MISSING accept-encoding HEADER --------------\n\
+  \\n\
+  \Add the --compressed flag to help reduce bandwidth costs!\n\
+  \\n\
+  \    curl --compressed https://package.elm-lang.org" <> B.fromByteString uri <> "\n\
+  \\n\
+  \If that does not work for some reason, you can try the following:\n\
+  \\n\
+  \    curl -sH 'accept-encoding: gzip' https://package.elm-lang.org" <> B.fromByteString uri <> " | gunzip\n\
+  \\n\
+  \Server costs are paid by individual community members, not some big company, so\n\
+  \thank you for helping out like this!\n\n"
+
+
+toWgetError :: BS.ByteString -> B.Builder
+toWgetError uri =
+  "-- 406 -- MISSING accept-encoding HEADER --------------\n\
+  \\n\
+  \Please download the gzipped version to help reduce bandwidth costs!\n\
+  \\n\
+  \    wget --header=\"accept-encoding: gzip\" https://package.elm-lang.org" <> B.fromByteString uri <> " | gunzip\n\
+  \\n\
+  \Server costs are paid by individual community members, not some big company, so\n\
+  \thank you for helping out like this!\n\n"
