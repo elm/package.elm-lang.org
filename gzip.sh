6dd6a3e3647ca0f24f2c19d4e1b41fbc7b1d3f54
Evan Czaplicki <evancz@users.noreply.github.com>
2020-02-05 22:51:38 -0500

    do not bother gzipping releases.json
    It doesn't save that much, and I think it'll be annoying during
    publication

--- gzip.sh
+++ gzip.sh
@@ -13,15 +13,6 @@ function batchGzip {
     done
 }
 
-#                         # total savings
-#                         # 5 Feb 2020
-#                         #------------
-batchGzip "elm.json"      #   2,460,384
-batchGzip "docs.json"     # 204,728,592
-batchGzip "README.md"     #  11,402,288
-batchGzip "releases.json" #      94,960
-#                         #------------
-#                         # 218,686,224
-
-# before:  269,159,000
-# after:    50,472,776
+batchGzip "elm.json"
+batchGzip "docs.json"
+batchGzip "README.md"
