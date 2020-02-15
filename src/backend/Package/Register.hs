94ba49d28f5e9c8f266b6562e91c63c85d00f6ec
Evan Czaplicki <evancz@users.noreply.github.com>
2020-02-15 12:50:44 -0500

    read elm.json.gz properly
    not ideal, but other options feel somewhat less ideal

--- src/backend/Package/Register.hs
+++ src/backend/Package/Register.hs
@@ -7,7 +7,7 @@ module Package.Register
 
 
 import qualified Control.Exception as Ex
-import Control.Monad (when)
+import Control.Monad ((>=>), when)
 import Control.Monad.Trans (liftIO)
 import qualified Data.ByteString as BS
 import qualified Data.ByteString.Char8 as BSC
@@ -185,7 +185,7 @@ uploadFiles pkg vsn time =
               requireFile pkg vsn "elm.json" files
               requireFile pkg vsn "docs.json" files
               requireHash pkg vsn dir files
-              bytes <- liftIO $ BS.readFile (dir </> "elm.json")
+              bytes <- liftIO $ readElmJson dir
               case D.fromByteString Outline.decoder bytes of
                 Left _ ->
                   revert pkg vsn $ "Invalid content in elm.json file."
@@ -240,6 +240,17 @@ handlePart pkg vsn dir info stream =
 
     path ->
       return $ Left $ "Did not recognize " ++ show path ++ " part in form-data"
+
+
+readElmJson :: FilePath -> IO BS.ByteString
+readElmJson dir =
+  SF.withFileAsInput (dir </> "elm.json.gz") (SZ.gunzip >=> unpack "")
+  where
+    unpack bytes input =
+      do  maybeChunk <- SC.read input
+          case maybeChunk of
+            Just chunk -> unpack (bytes <> chunk) input
+            Nothing    -> return bytes
 
 
 
