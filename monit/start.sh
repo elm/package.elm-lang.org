967afbab7e1c05a8751ba85a9f7e161f69875891
Evan Czaplicki <evancz@users.noreply.github.com>
2020-02-15 13:55:24 -0500

    update monit/start.sh for newer build process

--- monit/start.sh
+++ monit/start.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 
 USER=xxxx
-PATH=/home/$USER/compiler/dist/build/elm/:$PATH
 API_KEY=12345
 
-cd /home/$USER/websites/package/
+cd /home/$USER/package.elm-lang.org
 
 ./dist/build/run-server/run-server --github=$API_KEY --port=8019 &
 
